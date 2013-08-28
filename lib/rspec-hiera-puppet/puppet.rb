99e44cfc343e7b6b3b2a5fa3571d1133a4d2823b
Maksym Melnychok <maksym.melnychok@gmail.com>
2013-08-28 15:17:49 +0200

    use spec data to override hiera stuff

--- lib/rspec-hiera-puppet/puppet.rb
+++ lib/rspec-hiera-puppet/puppet.rb
@@ -1,5 +1,6 @@
 require 'puppet'
-require 'hiera_puppet'
+require 'hiera'
+require 'hiera/scope'
 
 class Puppet::Parser::Compiler
   alias_method :compile_unadorned, :compile
@@ -8,10 +9,10 @@ class Puppet::Parser::Compiler
     spec = Thread.current[:spec]
 
     if spec
-      register_function(:hiera, :priority, :type => :rvalue)
-      register_function(:hiera_array, :array, :type => :rvalue)
-      register_function(:hiera_hash, :hash, :type => :rvalue)
-      register_function(:hiera_include, :array) do |value|
+      register_function(spec, :hiera, :priority, :type => :rvalue)
+      register_function(spec, :hiera_array, :array, :type => :rvalue)
+      register_function(spec, :hiera_hash, :hash, :type => :rvalue)
+      register_function(spec, :hiera_include, :array) do |value|
         method = Puppet::Parser::Functions.function(:include)
         send(method, value)
       end
@@ -20,10 +21,20 @@ class Puppet::Parser::Compiler
     compile_unadorned
   end
 
-  def register_function(name, resolution, options={})
+  def register_function(spec, name, resolution, options={})
     Puppet::Parser::Functions.newfunction(name, options) do |*args|
-      key, default, override = HieraPuppet.parse_args(args)
-      answer = HieraPuppet.lookup(key, default, self, override, resolution)
+      args = args[0]if args[0].is_a?(Array)
+
+      key, default, override = *args
+      hiera = Hiera.new(:config => spec.hiera_config.merge(:logger => 'puppet'))
+      hiera_scope = self.respond_to?("[]") ? self : Hiera::Scope.new(self)
+      answer = hiera.lookup(key, default, hiera_scope, override, resolution)
+
+      if answer.nil?
+        raise(Puppet::ParseError,
+          "Could not find data item #{key} in any Hiera data file and no default supplied")
+      end
+
       block_given? ? yield(answer) : answer
     end
   end
--- lib/rspec-hiera-puppet/version.rb
+++ lib/rspec-hiera-puppet/version.rb
@@ -1,7 +1,7 @@
 module Rspec
   module Hiera
     module Puppet
-      VERSION = "1.0.0"
+      VERSION = "0.3.1"
     end
   end
 end
